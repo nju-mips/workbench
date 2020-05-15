e7d3cc118ee94af850d67c16b199f36eb8741ad6
ouxianfei <ouxianfei@smail.nju.edu.cn>
2020-05-15 18:54:05 +0800

    fix the implementation of Divider

--- core/top.scala
+++ core/top.scala
@@ -15,7 +15,6 @@ class SimDev extends BlackBox {
   })
 }
 
-/* randomly delay the results returned from device */
 class DeviceAccessor extends Module {
   val io = IO(new Bundle {
     val clock = Input(Clock())
@@ -43,27 +42,23 @@ class DeviceAccessor extends Module {
   } else {
     io.in <> dev.io.in
   }
-
-  def dump():Unit = {
-    printv(this, "DEV")
-    printv(io.in, "DEV.in")
-  }
 }
 
 class Divider extends Module {
   val io = IO(Flipped(new DividerIO))
-  val dividend = io.data_dividend_bits.asSInt
-  val divisor = io.data_divisor_bits.asSInt
+  val dividend = io.data_dividend_tdata.asSInt
+  val divisor = io.data_divisor_tdata.asSInt
   val quotient = (dividend / divisor).asUInt
   val remainder = (dividend % divisor).asUInt
   require(quotient.getWidth == 40)
   require(remainder.getWidth == 40)
-  val pipe = Pipe(Y, Cat(quotient, remainder), conf.div_stages)
+  val pipe = Pipe(io.data_dividend_tvalid && io.data_divisor_tvalid,
+    Cat(quotient, remainder), conf.div_stages)
 
-  io.data_dividend_ready := Y
-  io.data_divisor_ready := Y
-  io.data_dout_valid := pipe.valid
-  io.data_dout_bits := pipe.bits
+  io.data_dividend_tready := Y
+  io.data_divisor_tready := Y
+  io.data_dout_tvalid := pipe.valid
+  io.data_dout_tdata := pipe.bits
 }
 
 class Multiplier extends Module {
